7940cc2d2e4c9410ad81f1f7513b10a709e3b291
Frederico F. Martins <fred.martins94@gmail.com>
2023-06-26 23:02:59 +0100

    Create discjarvis.py
    Python3 script.

---
+++ discjarvis.py
@@ -0,0 +1,9 @@
+import discord
+import time
+import random
+from datetime import datetime
+from discord.ext import commands, tasks
+from discord import activity
+import asyncio
+import requests
+import json
